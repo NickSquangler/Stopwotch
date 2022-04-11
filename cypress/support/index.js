da6249edc7e4ceb296277952785008fa47158eac
NickSquangler <62999723+NickSquangler@users.noreply.github.com>
2022-04-11 19:23:17 -0300

    Update index.js

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -16,5 +16,37 @@
 // Import commands.js using ES2015 syntax:
 import './commands'
 
+body {
+    background: #ffffff;
+    color: rgb(0, 0, 0);
+}
+button {
+    width: 100px;
+    height: 30px;
+    border: 3px soldi rgb(0, 0, 0);
+    border-radius: 50px;
+    background: #ffffff;
+    color: rgb(0, 0, 0);
+    cursor: pointer;
+    outline: none;
+}
+
+#stopwatch {
+    font-size: 125px;
+    position: absolute;
+    top: 45%;
+    left: 50%;
+    transform: translate(-50%, -55%);
+}
+#buttons {
+    position: absolute;
+    top: 55%;
+    left: 48.4%;
+    transform: translate(-51.6%, -45%);
+}
+#buttons li {
+    display: inline;
+    padding-left: 10px;
+}
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
